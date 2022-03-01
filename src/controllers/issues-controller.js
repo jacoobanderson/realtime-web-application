a1f88dfa1aa45a430b460a41781632396657979d
jacoobanderson <j00cwa@gmail.com>
2022-03-02 00:32:03 +0100

    Fix access to gitlab api

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -1,2 +1,26 @@
+import fetch from 'node-fetch'
+
 export class IssuesController {
+    async getIndex(req, res, next) {
+        try {
+            const issues = await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues`, {
+                headers: {
+                    authorization: `bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
+                }
+            })
+            const jsonIssues = await issues.json()
+
+            const viewData = {
+                issues: jsonIssues.map(issue => ({
+                    id: issue.id,
+                    title: issue.title,
+                    description: issue.description
+                }))
+            }
+            console.log(viewData)
+            res.render('issues/index', { viewData })
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/routes/issues-router.js
+++ src/routes/issues-router.js
@@ -1,10 +1,10 @@
 import express from 'express'
-// import { IssuesController } from '../controllers/issues-controller.js'
+import { IssuesController } from '../controllers/issues-controller.js'
 
 export const router = express.Router()
 
-// const controller = new IssuesController()
+const controller = new IssuesController()
 
-// router.get('/', (req, res, next) => controller.index(req, res, next))
+router.get('/', (req, res, next) => controller.getIndex(req, res, next))
 // router.get('/:id/close', (req, res, next) => controller.close(req, res, next))
 // router.get('/:id/edit', (req, res, next) => controller.edit(req, res, next))
