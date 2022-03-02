fbae1ecb1fd20364f1b910242f0cc4c55908a917
jacoobanderson <j00cwa@gmail.com>
2022-03-02 17:07:42 +0100

    Add reopen issue functionality

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -38,4 +38,43 @@ export class IssuesController {
             next(error)
         }
     }
+
+    async getClosedIssues(req, res, next) {
+        try {
+            const issues = await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues?state=closed`, {
+                headers: {
+                    authorization: `bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
+                }
+            })
+            const jsonIssues = await issues.json()
+
+            const viewData = {
+                issues: jsonIssues.map(issue => ({
+                    id: issue.id,
+                    iid: issue.iid,
+                    title: issue.title,
+                    avatar: issue.author.avatar_url,
+                    description: issue.description
+                }))
+            }
+            res.render('issues/closedIssues', { viewData })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async reOpenIssue(req, res, next) {
+        try {
+            await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues/${req.body.iid}?state_event=reopen`, {
+                method: 'PUT',
+                headers: {
+                    authorization: `bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
+                }
+            })
+            console.log(res.statusCode)
+            res.redirect('..')
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/routes/issues-router.js
+++ src/routes/issues-router.js
@@ -6,5 +6,6 @@ export const router = express.Router()
 const controller = new IssuesController()
 
 router.get('/', (req, res, next) => controller.getIndex(req, res, next))
+router.get('/closed', (req, res, next) => controller.getClosedIssues(req, res, next))
 router.post('/:id/close', (req, res, next) => controller.closeIssue(req, res, next))
-// router.get('/:id/edit', (req, res, next) => controller.edit(req, res, next))
+router.post('/:id/reopen', (req, res, next) => controller.reOpenIssue(req, res, next))
