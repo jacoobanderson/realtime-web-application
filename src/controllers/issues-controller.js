cd1718ee9df32d673bf2c37012b5e5e088a253b7
jacoobanderson <j00cwa@gmail.com>
2022-03-02 16:11:27 +0100

    Fix close issue functionality

--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 export class IssuesController {
     async getIndex(req, res, next) {
         try {
-            const issues = await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues`, {
+            const issues = await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues?state=opened`, {
                 headers: {
                     authorization: `bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
                 }
@@ -13,13 +13,28 @@ export class IssuesController {
             const viewData = {
                 issues: jsonIssues.map(issue => ({
                     id: issue.id,
+                    iid: issue.iid,
                     title: issue.title,
                     avatar: issue.author.avatar_url,
                     description: issue.description
                 }))
             }
-            console.log(viewData)
             res.render('issues/index', { viewData })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async closeIssue(req, res, next) {
+        try {
+            await fetch(`https://gitlab.lnu.se/api/v4/projects/${process.env.PROJECT_ID}/issues/${req.body.iid}?state_event=close`, {
+                method: 'PUT',
+                headers: {
+                    authorization: `bearer ${process.env.PERSONAL_ACCESS_TOKEN}`
+                }
+            })
+            console.log(res.statusCode)
+            res.redirect('..')
         } catch (error) {
             next(error)
         }
--- src/routes/issues-router.js
+++ src/routes/issues-router.js
@@ -6,5 +6,5 @@ export const router = express.Router()
 const controller = new IssuesController()
 
 router.get('/', (req, res, next) => controller.getIndex(req, res, next))
-// router.get('/:id/close', (req, res, next) => controller.close(req, res, next))
+router.post('/:id/close', (req, res, next) => controller.closeIssue(req, res, next))
 // router.get('/:id/edit', (req, res, next) => controller.edit(req, res, next))
